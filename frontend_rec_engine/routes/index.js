47572e409bd21bbb21427508baf147e853d40b22
yongbin999 <yongbinliang@umass.edu>
2015-04-25 23:10:32 -0400

    finished new interface

--- frontend_rec_engine/routes/index.js
+++ frontend_rec_engine/routes/index.js
@@ -7,11 +7,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('single_circle', { title: 'Express' });
 });
 
 router.get('/sample_data', function(req, res, next) {
   res.render('sample_data', { title: 'Express' });
 });
+
+router.get('/template', function(req, res, next) {
+  res.render('template', { title: 'Express' });
+});
+
+router.get('/iframe', function(req, res, next) {
+  res.render('iframe', { title: 'Express' });
+});
+router.get('/aboutus', function(req, res, next) {
+  res.render('aboutus', { title: 'Express' });
+});
+
 
 module.exports = router;
