10d0ca1aecc910273009b264f5cfee43989c555d
samdeanefox <samdeanefox@gmail.com>
2015-04-25 20:38:55 -0400

    added timer

--- backend_python/Rec-Engine.py
+++ backend_python/Rec-Engine.py
@@ -1,3 +1,10 @@
 from methods import *
+import time
+
+time_start = time.time()
 
 print jsonConvert('subset.json')
+
+time_end = time.time()
+total_time = time_end - time_start
+print '\nRuntime: ' + str(total_time)
