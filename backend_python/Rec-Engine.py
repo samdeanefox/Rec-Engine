c855728c237a4a4203f334b83023c4c6284c6a15
samdeanefox <samdeanefox@gmail.com>
2015-04-26 17:13:47 -0400

    generator working

--- backend_python/Rec-Engine.py
+++ backend_python/Rec-Engine.py
@@ -3,8 +3,32 @@ import time
 
 time_start = time.time()
 
-print jsonConvert('subset.json')
+dataset = jsonConvert('newfile.json')
+dictionary = {}
+entries = 0
+tries = 0
 
 time_end = time.time()
 total_time = time_end - time_start
+
+#Initialize dictionary
+for i in dataset:
+	if 'Ambience' in  i['attributes']:
+		dictionary[i['name']] = i['attributes']['Ambience']
+		entries = entries + 1
+	tries = tries + 1
+
+#Calculate score
+print topMatches(dictionary, 'Taipei South', n=1000)
+
+
+
+
+
+
+
+print "Tries: "
+print tries
+print "Entries: "
+print entries
 print '\nRuntime: ' + str(total_time)
--- backend_python/methods.py
+++ backend_python/methods.py
@@ -1,4 +1,5 @@
-import json;
+import json
+from math import sqrt
 
 def jsonConvert(file_descriptor):
 	data = []
@@ -6,3 +7,34 @@ def jsonConvert(file_descriptor):
 	    for line in f:
 	        data.append(json.loads(line))
 	return data
+
+def getScore(dict,p1,p2):
+ 
+  si={}
+  for item in dict[p1]:
+    if item in dict[p2]: si[item]=1
+  
+  n=len(si)
+  
+  if n==0: return 0
+  
+  sum1=sum([dict[p1][it] for it in si])
+  sum2=sum([dict[p2][it] for it in si])
+  
+  sum1Sq=sum([pow(dict[p1][it]+1,2) for it in si])
+  sum2Sq=sum([pow(dict[p2][it]+1,2) for it in si])
+  
+  pSum=sum([dict[p1][it]*dict[p2][it] for it in si])
+
+  num=pSum-(sum1*sum2/n)
+  den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+  if den==0: return 0
+  r=num/den
+  return r
+
+def topMatches(dict,person,n=5,similarity=getScore):
+	scores=[(similarity(dict,person,other),other)
+                       for other in dict if other!=person]
+  	scores.sort()
+  	scores.reverse()
+  	return scores[0:n]
