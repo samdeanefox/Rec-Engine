07fd08318c82341e4a5808e9f0b08206c88622d7
yongbin999 <yongbinliang@umass.edu>
2015-04-25 21:37:40 -0400

    added bootstrap framwork

--- frontend_rec_engine/routes/index.js
+++ frontend_rec_engine/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/sample_data', function(req, res, next) {
+  res.render('sample_data', { title: 'Express' });
 });
 
 module.exports = router;
